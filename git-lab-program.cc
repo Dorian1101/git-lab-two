f23654a9df2af76bee7a4ccf751f769f7b995dc0
Dorian1101 <dorianwilliams1101@gmail.com>
2024-05-17 21:57:25 -0400

    Done

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Dorian Williams
+ *        Date: May 17 2024
+ * Description: Introductory lab assignment
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+cout << "Hello Git!!" << endl;    return 0;
+}// main
